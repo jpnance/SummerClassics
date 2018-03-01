0a5884d086220a75810f22253426b4f4e75061f2
Patrick Nance <jpnance@gmail.com>
2018-02-28 21:29:15 -0800

    Adding a very basic placeholder page that really just proves that my static files are getting served properly.

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+app.set('view engine', 'pug');
+
 app.get('/', function(request, response) {
-	response.send('Summer Classix');
+	response.render('placeholder');
 });
 
 process.env.PORT = process.env.PORT || 3333;
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, function() { console.log('yes, listening on port ' + process.env.PORT) });
