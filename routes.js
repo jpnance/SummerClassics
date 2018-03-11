bef851ebc3f9d3c313751a238fc0e269b79aefb9
Patrick Nance <jpnance@gmail.com>
2018-03-10 18:03:35 -0800

    Adding a route for viewing only a certain team's schedule.

--- routes.js
+++ routes.js
@@ -2,4 +2,5 @@ var schedule = require('./services/schedule');
 
 module.exports = function(app) {
 	app.get('/', schedule.showAll);
+	app.get('/schedule/:teamAbbreviation', schedule.showAllForTeam);
 };
--- services/schedule.js
+++ services/schedule.js
@@ -1,13 +1,30 @@
+var Game = require('../models/Game');
 var Team = require('../models/Team');
 
 module.exports.showAll = function(request, response) {
 	var data = [
-		Team.find().sort('league division teamName')
+		Game.find().sort('startTime').populate('away.team').populate('home.team')
 	];
 
 	Promise.all(data).then(function(values) {
-		var teams = values[0];
+		var games = values[0];
 
-		response.render('placeholder', { teams: teams });
+		response.render('placeholder', { games: games });
+	});
+};
+
+module.exports.showAllForTeam = function(request, response) {
+	var data = [
+		new Promise(function(resolve, reject) {
+			Team.findOne({ abbreviation: request.params.teamAbbreviation }, function(error, team) {
+				resolve(Game.find({ '$or': [ { 'home.team': team._id }, { 'away.team': team._id } ]}).sort('startTime').populate('away.team home.team'));
+			});
+		})
+	];
+
+	Promise.all(data).then(function(values) {
+		var games = values[0];
+
+		response.render('placeholder', { games: games });
 	});
 };
