8d2c6bc584a97aef4e085910000161a01e04e2d1
Patrick Nance <jpnance@gmail.com>
2018-03-19 19:07:06 -0700

    Adding a couple of dumb routes.

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ var classics = require('./services/classics');
 
 module.exports = function(app) {
 	app.get('/', schedule.showAllForDate);
+	app.get('/schedule/?', schedule.showAllForDate);
 	app.get('/picks', classics.showAllForUser);
 	app.get('/pick/:teamId/:gameId', classics.pick);
 	app.get('/schedule/:date', schedule.showAllForDate);
