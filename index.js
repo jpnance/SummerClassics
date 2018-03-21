4984184ae9e2fa30221332baf4008db495f1b593
Patrick Nance <pnance@newengen.com>
2018-03-21 07:13:03 -0700

    Adding middleware that we need for parsing cookies and form data.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
