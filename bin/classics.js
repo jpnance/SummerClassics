2f18c8c32be29d5f226779febe564cae62932160
Patrick Nance <jpnance@gmail.com>
2018-03-30 19:11:05 -0700

    Adding a proc for manual re-scoring, just in case the need arises.

---
+++ bin/classics.js
@@ -0,0 +1,28 @@
+var Classic = require('../models/Classic');
+
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI);
+
+Classic.find({ season: process.env.SEASON }).populate('picks').exec(function(error, classics) {
+	var classicPromises = [];
+
+	classics.forEach(function(classic) {
+		classicPromises.push(new Promise(function(resolve, reject) {
+			classic.tally();
+
+			classic.save(function(error) {
+				if (!error) {
+					resolve('good');
+				}
+				else {
+					reject(error);
+				}
+			});
+		}));
+	});
+
+	Promise.all(classicPromises).then(function() {
+		mongoose.disconnect();
+	});
+});
