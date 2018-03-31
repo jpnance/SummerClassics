3344ba6a71f8e8dda96615ca48a31fd71ac62b1b
Patrick Nance <pnance@newengen.com>
2018-03-30 23:18:53 -0700

    Untemporary fix.

--- bin/games.js
+++ bin/games.js
@@ -12,7 +12,7 @@ mongoose.connect(process.env.MONGODB_URI);
 var dateCushion = new Date();
 dateCushion.setDate(dateCushion.getDate() + 7);
 
-Game.find({ status: { '$nin': ['FT', 'CR', 'FR', 'CI', 'FG'] }, startTime: { '$lt': dateCushion } }).sort('startTime').exec(function(error, games) {
+Game.find({ status: { '$nin': ['F', 'FT', 'CR', 'FR', 'CI', 'FG'] }, startTime: { '$lt': dateCushion } }).sort('startTime').exec(function(error, games) {
 	var gamePromises = [];
 
 	games.forEach(function(game) {
