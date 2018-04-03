0ee5607b8bee116b23d1b7d2a82767aa7c21b578
Patrick Nance <pnance@newengen.com>
2018-04-03 16:28:12 -0700

    I think this is actually better to let game objects just know how to sync themselves.

--- bin/games.js
+++ bin/games.js
@@ -1,8 +1,6 @@
 var request = require('superagent');
 
-var Status = require('../models/Status');
 var Game = require('../models/Game');
-var Player = require('../models/Player');
 var Classic = require('../models/Classic');
 
 var mongoose = require('mongoose');
@@ -16,85 +14,7 @@ Game.find({ status: { '$nin': ['F', 'FT', 'CR', 'FR', 'CI', 'FG'] }, startTime: 
 	var gamePromises = [];
 
 	games.forEach(function(game) {
-		gamePromises.push(new Promise(function(resolve, reject) {
-			request.get('https://statsapi.mlb.com/api/v1.1/game/' + game._id + '/feed/live', function(error, response) {
-				var playerPromises = [];
-
-				var data = JSON.parse(response.text);
-
-				if (!data.liveData || !data.liveData.linescore || !data.liveData.linescore.teams) {
-					resolve('fine');
-					return;
-				}
-
-				if (data.gameData.probablePitchers) {
-					var pitcherIds = [];
-
-					if (data.gameData.probablePitchers.away) {
-						game.away.probablePitcher = data.gameData.probablePitchers.away.id;
-						pitcherIds.push(data.gameData.probablePitchers.away.id);
-					}
-					if (data.gameData.probablePitchers.home) {
-						game.home.probablePitcher = data.gameData.probablePitchers.home.id;
-						pitcherIds.push(data.gameData.probablePitchers.home.id);
-					}
-
-					pitcherIds.forEach(function(pitcherId) {
-						playerPromises.push(new Promise(function(resolve2, reject2) {
-							request.get('https://statsapi.mlb.com/api/v1/people/' + pitcherId, function(error, response) {
-								var playerData = JSON.parse(response.text);
-								var player = playerData.people[0];
-
-								var newPlayer = {
-									number: player.primaryNumber,
-									name: player.fullName,
-									position: player.primaryPosition.abbreviation,
-									bats: player.batSide.code,
-									throws: player.pitchHand.code
-								};
-
-								Player.findByIdAndUpdate(player.id, newPlayer, { upsert: true }).then(function() {
-									resolve2('good');
-								});
-							});
-						}));
-					});
-				}
-
-				game.status = data.gameData.status.statusCode;
-
-				playerPromises.push(Status.update(data.gameData.status, { '$set': { example: game._id } }, { upsert: true }));
-
-				if (game.status == 'I' || game.status == 'MA' || game.status == 'O' || game.status == 'F') {
-					game.away.score = data.liveData.linescore.teams.away.runs;
-					game.home.score = data.liveData.linescore.teams.home.runs;
-
-					game.inning.number = data.liveData.linescore.currentInning;
-					game.inning.ordinal = data.liveData.linescore.currentInningOrdinal;
-					game.inning.state = data.liveData.linescore.inningState;
-					game.inning.half = data.liveData.linescore.inningHalf;
-				}
-
-				if (game.status == 'F') {
-					if (game.away.score > game.home.score) {
-						game.away.winner = true;
-						game.home.winner = false;
-					}
-					else if (game.home.score > game.away.score) {
-						game.home.winner = true;
-						game.away.winner = false;
-					}
-				}
-
-				Promise.all(playerPromises).then(function() {
-					game.save(function(error) {
-						if (!error) {
-							resolve('good');
-						}
-					});
-				});
-			});
-		}));
+		gamePromises.push(game.syncWithApi());
 	});
 
 	Promise.all(gamePromises).then(function() {
--- models/Game.js
+++ models/Game.js
@@ -66,6 +66,94 @@ gameSchema.methods.isOver = function() {
 	return this.status == 'O' || this.status == 'F';
 };
 
+gameSchema.methods.syncWithApi = function() {
+	var thisGame = this;
+
+	return new Promise(function(resolve, reject) {
+		var request = require('superagent');
+
+		var Status = require('../models/Status');
+
+		request.get('https://statsapi.mlb.com/api/v1.1/game/' + thisGame._id + '/feed/live', function(error, response) {
+			var playerPromises = [];
+
+			var data = JSON.parse(response.text);
+
+			if (!data.liveData || !data.liveData.linescore || !data.liveData.linescore.teams) {
+				resolve('fine');
+				return;
+			}
+
+			if (data.gameData.probablePitchers) {
+				var pitcherIds = [];
+
+				if (data.gameData.probablePitchers.away) {
+					thisGame.away.probablePitcher = data.gameData.probablePitchers.away.id;
+					pitcherIds.push(data.gameData.probablePitchers.away.id);
+				}
+				if (data.gameData.probablePitchers.home) {
+					thisGame.home.probablePitcher = data.gameData.probablePitchers.home.id;
+					pitcherIds.push(data.gameData.probablePitchers.home.id);
+				}
+
+				pitcherIds.forEach(function(pitcherId) {
+					playerPromises.push(new Promise(function(resolve2, reject2) {
+						request.get('https://statsapi.mlb.com/api/v1/people/' + pitcherId, function(error, response) {
+							var playerData = JSON.parse(response.text);
+							var player = playerData.people[0];
+
+							var newPlayer = {
+								number: player.primaryNumber,
+								name: player.fullName,
+								position: player.primaryPosition.abbreviation,
+								bats: player.batSide.code,
+								throws: player.pitchHand.code
+							};
+
+							Player.findByIdAndUpdate(player.id, newPlayer, { upsert: true }).then(function() {
+								resolve2('good');
+							});
+						});
+					}));
+				});
+			}
+
+			thisGame.status = data.gameData.status.statusCode;
+
+			playerPromises.push(Status.update(data.gameData.status, { '$set': { example: thisGame._id } }, { upsert: true }));
+
+			if (thisGame.status == 'I' || thisGame.status == 'MA' || thisGame.status == 'O' || thisGame.status == 'F') {
+				thisGame.away.score = data.liveData.linescore.teams.away.runs;
+				thisGame.home.score = data.liveData.linescore.teams.home.runs;
+
+				thisGame.inning.number = data.liveData.linescore.currentInning;
+				thisGame.inning.ordinal = data.liveData.linescore.currentInningOrdinal;
+				thisGame.inning.state = data.liveData.linescore.inningState;
+				thisGame.inning.half = data.liveData.linescore.inningHalf;
+			}
+
+			if (thisGame.status == 'F') {
+				if (thisGame.away.score > thisGame.home.score) {
+					thisGame.away.winner = true;
+					thisGame.home.winner = false;
+				}
+				else if (thisGame.home.score > thisGame.away.score) {
+					thisGame.home.winner = true;
+					thisGame.away.winner = false;
+				}
+			}
+
+			Promise.all(playerPromises).then(function() {
+				thisGame.save(function(error) {
+					if (!error) {
+						resolve('good');
+					}
+				});
+			});
+		});
+	});
+};
+
 gameSchema.statics.progressSortWithPopulatedTeams = function(a, b) {
 	if (a.isFinalAndCool() && !b.isFinalAndCool()) {
 		return 1;
