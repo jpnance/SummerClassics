99b488a0558dea49c6c3fd11c61e4baad1ebf2e0
Patrick Nance <jpnance@gmail.com>
2018-04-06 07:25:18 -0700

    Making the init proc work a bit better but also removing it from Procfile because it's kinda dangerous.

--- bin/init.js
+++ bin/init.js
@@ -7,22 +7,28 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI);
 
-var user = new User({
-	username: process.env.ADMIN_USERNAME,
-	password: crypto.createHash('sha256').update(process.env.ADMIN_PASSWORD).digest('hex'),
-	firstName: process.env.ADMIN_FIRST_NAME,
-	lastName: process.env.ADMIN_LAST_NAME,
-	displayName: process.env.ADMIN_DISPLAY_NAME,
-	seasons: [ process.env.SEASON ],
-	admin: true
-});
+console.log(process.env.ADMIN_USERNAME);
 
-user.save(function(error) {
-	if (error) {
-		console.log(error);
+User.findOne({ username: process.env.ADMIN_USERNAME }).then(function(user) {
+	if (!user) {
+		user = new User({
+			username: process.env.ADMIN_USERNAME,
+			password: crypto.createHash('sha256').update(process.env.ADMIN_PASSWORD).digest('hex'),
+			firstName: process.env.ADMIN_FIRST_NAME,
+			lastName: process.env.ADMIN_LAST_NAME,
+			displayName: process.env.ADMIN_DISPLAY_NAME,
+			seasons: [ process.env.SEASON ],
+			admin: true
+		});
 	}
 
-	Classic.initialize(user, process.env.SEASON).then(function() {
-		mongoose.disconnect();
+	user.save(function(error) {
+		if (error) {
+			console.log(error);
+		}
+
+		Classic.initialize(user, process.env.SEASON).then(function() {
+			mongoose.disconnect();
+		});
 	});
 });
