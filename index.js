c030b5271d44e126254220e1e44e4296428f766f
Patrick Nance <jpnance@gmail.com>
2018-04-30 14:02:47 -0700

    This might count as swinging too heavy a hammer but 'May' shouldn't have a period after it in an abbreviated context.

--- index.js
+++ index.js
@@ -13,7 +13,14 @@ app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
-app.locals.dateFormat = require('dateformat');
+var dateFormat = require('dateformat');
+
+dateFormat.i18n.monthNames = [
+	'Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'Jun.', 'Jul.', 'Aug.', 'Sep.', 'Oct.', 'Nov.', 'Dec.',
+	'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+];
+
+app.locals.dateFormat = dateFormat;
 
 require('./routes')(app);
 
