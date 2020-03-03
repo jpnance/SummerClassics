f110cf3a4ba8893f6be91155e131e214e1bd2d27
Patrick Nance <jpnance@gmail.com>
2020-03-03 07:32:01 -0800

    Yeah, why not. Let's just upgrade a bunch of stuff and see if it works.

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ require('./routes')(app);
 
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 process.env.PORT = process.env.PORT || 3333;
 
