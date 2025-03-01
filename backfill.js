e0a5313d56a8b52bb896c20271721343806688ab
Patrick Nance <jpnance@gmail.com>
2025-02-28 21:10:28 -0800

    Seems like it works well.

---
+++ backfill.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv').config({ path: '/app/.env' });
+
+const mongoose = require('mongoose');
+const mongoUri = process.env.MONGODB_URI || null;
+
+mongoose.connect(mongoUri);
+
+const User = require('./models/User');
+
+User.find({}).then(handleUsers).then(console.log).then(disconnect);
+
+function handleUsers(users) {
+	return Promise.all(users.map(convertUsername));
+}
+
+function disconnect() {
+	mongoose.disconnect();
+	process.exit();
+}
+
+function convertUsername(user) {
+	const { firstName, lastName } = user;
+	const newUsername = [firstName, lastName].join('-').toLowerCase().replaceAll(/[']/g, '');
+
+	user.username = newUsername;
+
+	return user.save();
+	//return Promise.resolve(newUsername);
+}
