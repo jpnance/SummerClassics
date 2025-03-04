bc8882251cd6b98cd3e91bf86fb65e2791a3143b
Patrick Nance <jpnance@gmail.com>
2025-03-03 22:13:54 -0800

    Extremely stupid stuff.

--- bin/logos.sh
+++ bin/logos.sh
@@ -3,7 +3,7 @@
 teams=(
 	'tb' 'bos' 'nyy' 'tor' 'bal'
 	'chw' 'cle' 'det' 'kc' 'min'
-	'hou' 'sea' 'oak' 'laa' 'tex'
+	'hou' 'sea' 'ath' 'laa' 'tex'
 	'atl' 'phi' 'nym' 'mia' 'wsh'
 	'mil' 'stl' 'cin' 'chc' 'pit'
 	'sf' 'lad' 'sd' 'col' 'ari'
@@ -12,3 +12,6 @@ teams=(
 for team in "${teams[@]}"; do
 	curl "https://a.espncdn.com/combiner/i?img=/i/teamlogos/mlb/500/scoreboard/${team}.png&h=80&w=80" > ../public/images/${team}.png
 done
+
+mv ../public/images/ari.png ../public/images/az.png
+mv ../public/images/chw.png ../public/images/cws.png
